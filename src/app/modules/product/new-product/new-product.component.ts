8b4c9c1244b69fd991a2f84200db3a0e96bd1ec6
Daniel Esteban <decepeda@misena.edu.co>
2023-03-24 20:36:47 -0500

    Se crea el modulo para guardar un producto

---
+++ src/app/modules/product/new-product/new-product.component.ts
@@ -0,0 +1,80 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {CategoryService} from "../../shared/services/category.service";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {ProductService} from "../../shared/services/product.service";
+
+@Component({
+  selector: 'app-new-product',
+  templateUrl: './new-product.component.html',
+  styleUrls: ['./new-product.component.css']
+})
+export class NewProductComponent implements OnInit{
+
+  public  productForm: FormGroup;
+  estadoFormulario: string = '';
+  categories: Category[] = [];
+  selectedFile: any;
+  nameImage: string = "";
+  constructor(private formBuilder: FormBuilder,
+              private categoryService: CategoryService,
+              private productService: ProductService,
+              private dialogRef: MatDialogRef<NewProductComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
+    this.estadoFormulario = "Agregar";
+    this.productForm = this.formBuilder.group({
+      name: [ '', Validators.required ],
+      price: [ '', Validators.required ],
+      quantity: [ '', Validators.required ],
+      category: [ '', Validators.required ],
+      photo: [ '', Validators.required ]
+    })
+  }
+  ngOnInit() {
+    this.getCategories()
+  }
+  onSave(){
+
+    let data = {
+      name: this.productForm.get('name')?.value,
+      price: this.productForm.get('price')?.value,
+      quantity: this.productForm.get('quantity')?.value,
+      category: this.productForm.get('category')?.value,
+      photo: this.selectedFile
+    }
+
+    const uploadImageData = new FormData();
+    uploadImageData.append('photo', data.photo, data.photo.name );
+    uploadImageData.append('name', data.name);
+    uploadImageData.append('price', data.price);
+    uploadImageData.append('quantity', data.quantity);
+    uploadImageData.append('categoryId', data.category);
+
+    this.productService.saveProduct(uploadImageData).subscribe({
+      next: () => this.dialogRef.close(1),
+      error: () => this.dialogRef.close(2)
+    })
+  }
+  onCancel(){
+    this.dialogRef.close(3)
+  }
+  getCategories(){
+    this.categoryService.getCategories().subscribe({
+      next: (data: any) => {
+        this.categories = data.categoryResponse.categories
+      },
+      error: err => console.log('error al consultar las categorías', err)
+    })
+  }
+  onFileChanged( event: any ){
+    this.selectedFile = event.target.files[0];
+    console.log(this.selectedFile);
+    this.nameImage = event.target.files[0].name;
+  }
+}
+
+export interface Category {
+  description: string;
+  id: number;
+  name: string;
+}
